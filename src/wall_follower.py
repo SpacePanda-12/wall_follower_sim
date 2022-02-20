69b7feb890f60a39afe4c532b251f95d8faa1b88
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2022-02-20 16:33:42 -0500

    Add files via upload

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python2
-
 import numpy as np
-
 import rospy
 from rospy.numpy_msg import numpy_msg
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
+
 
 class WallFollower:
     # Import ROS parameters from the "params.yaml" file.
@@ -18,16 +17,35 @@ class WallFollower:
     DESIRED_DISTANCE = rospy.get_param("wall_follower/desired_distance")
 
     def __init__(self):
-        # TODO:
-        # Initialize your publishers and
-        # subscribers here
-
+        self.pub = rospy.Publisher('drive', AckermannDriveStamped, queue_size=10)
+        self.sub = rospy.Subscriber("/scan", LaserScan, self.callback)
+        self.rate = rospy.Rate(20)
         pass
 
-    # TODO:
-    # Write your callback functions here.
+    def callback(self, data):
+        if self.SIDE == 1:
+            lidar_data = np.array(data.ranges[90:135])
+        else:
+            lidar_data = np.array(data.ranges[270:315])
+
+        # 90 degrees corresponds to left, 270 corresponds to right
+        rospy.loginfo(data.ranges[270])
+        rospy.loginfo(data.ranges[90])
+        command = AckermannDriveStamped()
+        command.header.stamp = rospy.Time.now()
+        # TODO do I need to use a different frame?
+        command.header.frame_id = "base_link"
+        command.drive.steering_angle = 0
+        command.drive.steering_angle_velocity = 0
+        command.drive.speed = 0.5
+        command.drive.acceleration = 0.1
+        command.drive.jerk = 0
+        self.pub.publish(command)
+        self.rate.sleep()
+
 
 if __name__ == "__main__":
     rospy.init_node('wall_follower')
     wall_follower = WallFollower()
     rospy.spin()
+
