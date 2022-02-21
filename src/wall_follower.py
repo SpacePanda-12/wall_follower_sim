e69e39f2ac3f80880c1fe7fd2ad0b5cf3f7a426c
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2022-02-21 13:37:42 -0500

    Add files via upload

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -54,7 +54,7 @@ class WallFollower:
         # x = r cos(theta), y = r sin(theta)
         x_coords = np.multiply(lidar_data, np.cos(angles))
         y_coords = np.multiply(lidar_data, np.sin(angles))
-        VisualizationTools.plot_line(x_coords, y_coords, self.line_pub, frame="/laser", color="blue")
+        VisualizationTools.plot_line(x_coords, y_coords, self.line_pub, frame="/laser")
 
 
 
