12e3862e4f96d2e76c246058f53e3ad7caf89fb5
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2022-02-21 12:39:58 -0500

    Add files via upload

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -20,29 +20,64 @@ class WallFollower:
         self.pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=10)
         self.sub = rospy.Subscriber("/scan", LaserScan, self.callback)
         self.rate = rospy.Rate(20)
+        self.previous_time = 0
         pass
 
     def callback(self, data):
-        # if self.SIDE == 1:
-        #     lidar_data = np.array(data.ranges[90:135])
-        # else:
-        #     lidar_data = np.array(data.ranges[270:315])
+        angle_min = data.angle_min
+        angle_max = data.angle_max
+        angle_inc = data.angle_increment
+        if self.SIDE == -1:
+            # right side (negative angles)
+            # this includes data points 16 through 31; 32 is cut off, same as 83 in the other case. len(lidar_data) = 16
+            lidar_data = np.array(data.ranges[16:32])
+            start_angle = angle_min + 16 * angle_inc
 
-        # 90 degrees corresponds to left, 270 corresponds to right
-        rospy.loginfo("Right Side")
-        rospy.loginfo(data.ranges[67:83])
-        rospy.loginfo("left side")
-        rospy.loginfo(data.ranges[16:32])
-        # rospy.loginfo(data.ranges[270])
-        # rospy.loginfo(data.ranges[90])
+        else:
+            # left side (positive angles)
+            lidar_data = np.array(data.ranges[67:83])
+            start_angle = angle_min + 67 * angle_min
+
+        # Create desired linear regression for ideal path where the correct SIDE is DESIRED_DISTANCE from the wall
+        desired_data = []
+        angles = []
+        for i in range(16):
+            # Note: This works for both the left and right sides since the distances are the same and we
+            # are ignoring the specific angles here
+            desired_data[i] = self.DESIRED_DISTANCE/np.sin(angle_min + (16 + i) * angle_inc)
+            angles[i] = start_angle + angle_inc * i
+
+        desired_data = np.array(desired_data)
+        angles = np.array(angles)
+        # polyfit returns numpy array for y = mx+b; [m, b] is what is returned.
+        desired_lin_reg = np.polyfit(angles, desired_data, deg=1)
+        current_lin_reg = np.polyfit(angles, lidar_data, deg=1)
+
+        offset_error = current_lin_reg[1] - desired_lin_reg[1]
+        angle_error = current_lin_reg[0] - desired_lin_reg[0]
+
+        current_time = rospy.Time.now()
+        if self.previous_time == 0:
+            dt = 0
+        else:
+            dt = current_time - self.previous_time
+        self.previous_time = current_time
+
+        p_gain = 0.5
+        d_gain = 0.6
+        error = offset_error + angle_error
+
+        steering_angle_command = p_gain * error + d_gain * error / dt
+
+
         command = AckermannDriveStamped()
         command.header.stamp = rospy.Time.now()
         # TODO do I need to use a different frame?
         command.header.frame_id = "base_link"
-        command.drive.steering_angle = 0
+        command.drive.steering_angle = steering_angle_command
         command.drive.steering_angle_velocity = 0
-        command.drive.speed = 0.5
-        command.drive.acceleration = 0.1
+        command.drive.speed = self.VELOCITY
+        command.drive.acceleration = 0
         command.drive.jerk = 0
         self.pub.publish(command)
         self.rate.sleep()
